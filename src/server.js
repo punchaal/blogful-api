1b1162118846eea06445b807526221909ca90696
Pratik <punchaal@gmail.com>
2019-05-14 15:19:36 -0400

    Added post and delete functionality

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
+const knex = require('knex')
 const app = require('./app')
 
-const { PORT } = require('./config')
+const { PORT,DB_URL }= require('./config')
+
+const db = knex({
+  client:'pg',
+  connection: "postgresql://dunder-mifflin@localhost/blogful",
+})
+
+app.set('db',db)
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
